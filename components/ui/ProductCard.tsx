19b77eae108a02a216bf953e36b6c0e6254e2408
Enric <enric.carstens@gmail.com>
2023-07-07 14:32:20 +0200

    individual product page

--- components/ui/ProductCard.tsx
+++ components/ui/ProductCard.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import IconButton from "./IconButton";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "./Currency";
+import { useRouter } from "next/navigation";
 
 interface ProductCardProps {
     data: Product;
@@ -13,8 +14,14 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({
     data,
 }) => {
+    const router = useRouter();
+
+    const handleClick = () => {
+        router.push(`/product/${data.id}`);
+    }
+
     return (
-        <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+        <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
             <div className="aspect-square rounded-xl bg-gray-100 relative">
                 <Image alt="Image" src={data?.images?.[0]?.url} fill className="aspect-square object-cover rounded-md" />
                 <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
