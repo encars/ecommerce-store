0b533c560d177a2afd65d44ec6be200e80a88191
Enric <enric.carstens@gmail.com>
2023-07-07 14:04:52 +0200

    setup and featured products

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,25 @@
+import getBillboard from "@/actions/getBillboard";
+import getProducts from "@/actions/getProducts";
+import Billboard from "@/components/Billboard";
+import ProductList from "@/components/ProductList";
+import Container from "@/components/ui/Container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const products = await getProducts({ isFeatured: true });
+    const billboard = await getBillboard("6ac6fcbf-0a3b-45c4-adf7-821aafd546cb");
+
+    return (
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard} />
+            </div>
+            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                <ProductList title="Featured Products" items={products} />
+            </div>
+        </Container>
+    );
+};
+
+export default HomePage;
